7f6aa72faa89b028344b162bb76a510b71ab3955
Zhetchko Vitchev <notargetbg@gmail.com>
2018-03-22 20:53:35 +0200

    Initial commit - react app
    Some files added,
    Folder structure changed, added sass to the build pipeline

---
+++ src/Components/ScrambleGame.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/scrambleGame.css';
+
+class ScrambleGame extends Component {
+  render() {
+    return (
+      <div className="game-container">
+        <h1>Hi, I am a game!</h1>
+      </div>
+    );
+  }
+}
+
+export default ScrambleGame;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ScrambleGame from './Components/ScrambleGame';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<ScrambleGame />, document.getElementById('root'));
+// registerServiceWorker();
