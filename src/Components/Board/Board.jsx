112b2c255acbb177d5a19cd02ab538451a5017a8
Zhetchko Vitchev <notargetbg@gmail.com>
2018-03-23 09:49:43 +0200

    Add scrambled words to state

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -9,26 +9,34 @@ class Board extends Component {
 		this.state = {
 			hasGameStarted: false,
 			userInput: null,
-			answers: []
+			answers: [],
+			scrambledWords: [],
 		};
 	}
 
 	startGame = () => {
-		this.setState({hasGameStarted: true});
+		const firstScrambledWord = shuffle(words[0])
 		
+		this.setState({
+			hasGameStarted: true,
+			scrambledWords: [...this.state.scrambledWords, firstScrambledWord]
+		});
 		setTimeout(() => {
-			
-			console.log(this.state.answers);
-			console.log(words.map(answer => shuffle(answer)));
+
+			console.log(this.state)
 			console.log('ended');
 
 		}, 1000 * 5)
 	}
 
-	setAnswer = (e) => {
+	giveAnswer = (e) => {
 		e.preventDefault();				
+
+		const newScrambledWord = shuffle(words[this.state.scrambledWords.length]);
+
 		this.setState({
-            answers: [...this.state.answers, this.inputEl.value]
+			answers: [...this.state.answers, this.inputEl.value],
+			scrambledWords: [...this.state.scrambledWords, newScrambledWord]
 		});
 		this.inputEl.value = "";
 	}
@@ -41,8 +49,15 @@ class Board extends Component {
 
 	render() {
 		return (
-			<div className="roard">
-				<form onSubmit={this.setAnswer}>
+			<div className="board">
+				{this.state.scrambledWords && this.state.scrambledWords.length > 0 &&
+					this.state.scrambledWords.map((word) => (
+						<p>
+							{word}
+						</p>
+					))}
+
+				<form onSubmit={this.giveAnswer}>
 					<FormGroup bsSize="large">
 						<FormControl type="text" inputRef={(e) => {this.inputEl = e;}} placeholder="Enter an answer" onChange={this.updateInput} />
 						<Button bsStyle="primary" onClick={this.startGame}>Start game</Button>
